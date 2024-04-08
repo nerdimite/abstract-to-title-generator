2eee096b1c2700784d4cedf941408a53a66a69bd
Bhavesh Laddagiri <bhavesh.laddagiri1@gmail.com>
2024-04-08 23:08:08 +0530

    updated model endpoints

--- a2t-app/pages/index.js
+++ a2t-app/pages/index.js
@@ -1,46 +1,13 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Container, Paper, Label, Textarea, Button } from "../components/core";
 import ExamplesDropdown from "../components/ExamplesDropdown";
 
 export default function Home() {
   const [abstract, setAbstract] = useState();
-  const [defaultAbstract, setDefaultAbstract] = useState();
   const [title, setTitle] = useState("");
   const [status, setStatus] = useState("");
   const [loading, setLoading] = useState(false);
-
-  useEffect(async () => {
-    await load_model();
-  }, []);
-
-  const load_model = async () => {
-    setStatus(
-      "🚧 Loading Model into Memory, Please Wait for around 30 seconds..."
-    );
-    setLoading(true);
-
-    try {
-      const raw_response = await fetch(
-        "https://api.cellstrathub.com/nerdimite/abstract2title-v2",
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-            "x-api-key": "uH4mo8miig9mueJgbxsRa7pN0nryEXPb6gJTpBuL",
-          },
-          method: "GET",
-        }
-      );
-      const response = await raw_response.json();
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-    }
-
-    setStatus("⚡ Model is Ready");
-    setLoading(false);
-  };
 
   const predict = async () => {
     setStatus("⏳ The Model is Generating the Title");
@@ -48,20 +15,19 @@ export default function Home() {
 
     try {
       const raw_response = await fetch(
-        "https://api.cellstrathub.com/nerdimite/abstract2title-v2",
+        "https://qiyvjvwm57flqmhhawjq4t4y3u0djofs.lambda-url.us-east-1.on.aws/gradientfire/abstract2title",
         {
           headers: {
             "Content-Type": "application/json",
-            Accept: "application/json",
-            "x-api-key": "uH4mo8miig9mueJgbxsRa7pN0nryEXPb6gJTpBuL",
+            "x-api-key": "S76UrNGVw84VbOyaTMXvy1IUxVpKGZtz9GeSRVx2",
           },
           method: "POST",
-          body: abstract,
+          body: JSON.stringify(abstract),
         }
       );
       const response = await raw_response.json();
       console.log(response);
-      const output = response.body.output;
+      const output = response.output;
       setTitle(output);
     } catch (err) {
       console.log(err);
@@ -99,6 +65,21 @@ export default function Home() {
         <Paper>
           <div className="flex justify-between items-center mb-2">
             <Label>About the project</Label>
+            <a
+              href="https://github.com/nerdimite/abstract-to-title-generator"
+              target="_blank"
+            >
+              <div
+                className="py-2 px-3  inline-block rounded-full \
+                  bg-blue-200 text-sm hover:cursor-pointer hover:ring-1 hover:ring-blue-600 \
+                  transition ease-in-out duration-200"
+              >
+                <span className="font-semibold text-blue-600">
+                  Get Source Code{" "}
+                </span>
+                💻
+              </div>
+            </a>
           </div>
           <p className="mb-2 text-base text-gray-700">
             Writing a research paper is one thing and coming up with a catchy
